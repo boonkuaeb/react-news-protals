012739e745ad4ee449ee172ca32e9bf8ac76b0ad
Boonkuae Boonsutta <boonkuae.boo@ascendcorp.com>
2018-08-27 17:01:35 +0700

    int project

---
+++ src/components/News/NewsSingle.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+class NewsSingle extends Component {
+
+    render() {
+        return (
+            <li>
+                <p>item.title</p>
+            </li>
+        );
+    }
+}
+
+export default NewsSingle;
